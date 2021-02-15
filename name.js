d9036a79a2c633bdd63ebf6c5a316fbe5b314a9b
Brendan Allatta <brendan.allatta@gmail.com>
2021-02-15 13:59:45 -0500

    reverted to original after accidentally adding something to wrong code base

--- name.js
+++ name.js
@@ -7,7 +7,6 @@ const interf = readline.createInterface({
 
 function handleAnswer(answer) {
     // voila! Now you have a safe space to deal with what the user said.
-    console.log(answer)
     console.log(`It's nice to meet you, ${answer}`)
     interf.close()  
 }
