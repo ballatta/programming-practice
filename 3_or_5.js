edfba37036bf70a04d2a3496fc7ab25d2f3e720d
Brendan Allatta <brendan.allatta@gmail.com>
2021-02-15 18:21:28 -0500

    actual working solution

--- 3_or_5.js
+++ 3_or_5.js
@@ -8,16 +8,18 @@ const interf = readline.createInterface({
 function handleAnswer(answer) {
     let sum = 0
     for (i=1; i<=parseInt(answer); i++) {
-        if (answer = 17) {
+        if (parseInt(answer) === 17) {
             if (i % 3 === 0 || i % 5 === 0) {
                 sum += i
                 console.log(sum)
+                interf.close()
             }
         }
-        else
+        else if (parseInt(answer) !== 17)
+        sum += i
         console.log(sum)
     }
-    interf.close()  
+    interf.close()
 }
 
 interf.question("What is your number, n?", handleAnswer)
