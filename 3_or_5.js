fa2e9c10604dc90b6b96224996fc5eb88a0ec644
Brendan Allatta <brendan.allatta@gmail.com>
2021-02-15 17:09:04 -0500

    problem 5 progress

--- 3_or_5.js
+++ 3_or_5.js
@@ -8,11 +8,10 @@ const interf = readline.createInterface({
 function handleAnswer(answer) {
     let sum = 0
     for (i=1; i<=parseInt(answer); i++) {
-        sum += i;
-/*        if (answer = 17) {
-            (i % 3 === 0 && i % 5 === 0)
+        if (answer = 17) {
+            let multiples = (i % 3 === 0 | i % 5 === 0)
+            sum += multiples;
         }
-        */
         console.log(sum)
     }
     interf.close()  
