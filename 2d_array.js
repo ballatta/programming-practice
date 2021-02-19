afa1a047cf1fc42e00d67d1d2d42b138f596eb64
Jack DeVries <jdevries3133@gmail.com>
2021-02-19 00:00:00 +0000

    added more to do: 2d_array.js and endless_sum_or_product.js

---
+++ 2d_array.js
@@ -0,0 +1,22 @@
+/**
+ * You will need to use a data structure called a 2 Dimensional (2D) Array
+ * to represent the data structure for the 12-tone row project. In fact, your
+ * friend already has structured the data in a 2D array! To learn more about
+ * 2D arrays, first watch this YouTube video on the data structure in general:
+ *
+ *    https://www.youtube.com/watch?v=J1aQ9JN4vZY&t=75s
+ *
+ * Note that the code examples in the tutorial above are in a programming
+ * language called C. However, javascript is quite smiliar to C in terms of
+ * syntax. The biggest difference you will notice is that they talk a lot about
+ * "allocating," or "declaring," an array; because in C, you need to
+ * specifically tell the computer to create space in RAM to hold your data.
+ * Pretty wild! We don't need to worry about coding this in javascript, it
+ * happens automagically. So, if you see "int arr[4][3]," and are wondering
+ * what they are talking about, don't worry; just keep watching.
+ *
+ * Then, follow along with this tutorial which is specifically oriented towards
+ * javascript, and put your code and notes below.
+ *
+ *    https://www.youtube.com/watch?v=6m6QWuhLGZc
+ */
---
+++ endless_sum_or_product.js
@@ -0,0 +1,67 @@
+/**
+ * Nice solution! I want you to tweak this to further demonstrate your
+ * understanding of this pattern. Modify the program below such that
+ * it continues asking the questions forever until the user presses "e"
+ * to exit.
+ *
+ * For example:
+ *
+ * What is your number? (or type "e" to exit)
+ * INPUT: 5
+ * Sum or Factorial? (s/f)
+ * INPUT: s
+ * OUTPUT: 15
+ * What is your number? (or type "e" to exit)
+ * INPUT: 15
+ * Sum or Factorial? (s/f)
+ * INPUT: f
+ * OUTPUT: 1307674368000
+ * What is your number? (or type "e" to exit)
+ * INPUT: e
+ * (program exits)
+ *
+ *
+ * Note that in order to achieve this, you SHOULD NOT use a loop; neither
+ * a for or while loop. Instead, the best solution will use something called
+ * "recursion." Recursion is most simply defined as a function calling itself,
+ * or creating a loop of function calls. For example:
+ */
+
+/* Recursion example */
+const infinitelyRecurse = () => {
+  console.log("This will print forever");
+  infinitelyRecurse();
+};
+
+const readline = require("readline");
+
+const interf = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+interf.question("What is your number, n?", handleAnswer);
+
+let sum = 0;
+let product = 1;
+
+function handleAnswer(answer) {
+  let n = parseInt(answer);
+  for (i = 0; i <= n; i++) {
+    sum += i;
+  }
+  for (i = 1; i <= n; i++) {
+    product = product * i;
+  }
+  interf.question("Sum or Factorial? (s/f)", handleSecondAnswer);
+}
+
+function handleSecondAnswer(answer) {
+  if (answer === "s") {
+    console.log(sum);
+  } else if (answer === "f") {
+    console.log(product);
+  }
+  interf.close();
+}
+
