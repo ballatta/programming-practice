5c106427b76b2a77a742fb39c33eac960be0c1aa
Brendan Allatta <brendan.allatta@gmail.com>
2021-02-19 23:20:16 -0500

    working solution to problem 6

--- sum_or_product.js
+++ sum_or_product.js
@@ -5,16 +5,28 @@ const interf = readline.createInterface({
     output: process.stdout
   });
 
-function handleAnswer(answer) {
-    let sum = 0
-    let product = 0
-    let n = parseInt(answer)
-    for (i=1; i<=n; i++) {
-        if (answer2 = )
-        else
-        sum += i
-    console.log(sum)
-    interf.close()
-    }
 
 interf.question("What is your number, n?", handleAnswer)
+
+let sum = 0
+let product = 1
+
+function handleAnswer(answer) {
+    let n = parseInt(answer);
+    for (i = 0; i <= n; i++) {
+        sum += i;
+    }
+    for (i = 1; i <= n; i++) {
+        product += product * (i - 1)
+      }
+    interf.question('Sum or Factorial? (s/f)', handleSecondAnswer);
+}
+
+function handleSecondAnswer(answer) {
+        if (answer === 's') {
+            console.log(sum);
+        } else if (answer === 'f') {
+            console.log(product);
+        }
+    interf.close();
+}
